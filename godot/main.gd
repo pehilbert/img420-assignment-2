20d9aa0c39c700208cf45eb2007e765e45884f2f
pehilbert <pehilbert03@gmail.com>
2025-09-20 14:54:25 -0700

    Import assignment 1 and reconfigure build

--- godot/main.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-
-func _on_godot_sample_project_position_changed(node: Object, new_pos: Vector2) -> void:
-	print("The position of " + node.get_class() + " is now " + str(new_pos))
